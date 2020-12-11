94a8ea720ff86df82c85fa9284d2f4f4a611f459
Xuedong Wang <xuedongwang@tencent.com>
2020-12-11 17:42:22 +0800

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ class Home extends Component {
           <Col span={8}>
             <div className={style.box}>
               <div className={style.count}>
-                <div className={style.value}>13</div>
+                <div className={style.value}>89</div>
                 <div className={style.unit}>篇</div>
               </div>
               <div className={style.name}>文章</div>
@@ -23,18 +23,18 @@ class Home extends Component {
             <div className={style.box}>
               <div className={style.count}>
                 <div className={style.value}>13</div>
-                <div className={style.unit}>篇</div>
+                <div className={style.unit}>个</div>
               </div>
-              <div className={style.name}>文章</div>
+              <div className={style.name}>分类</div>
             </div>
           </Col>
           <Col span={8}>
             <div className={style.box}>
               <div className={style.count}>
-                <div className={style.value}>13</div>
-                <div className={style.unit}>篇</div>
+                <div className={style.value}>968</div>
+                <div className={style.unit}>条</div>
               </div>
-              <div className={style.name}>文章</div>
+              <div className={style.name}>评论</div>
             </div>
           </Col>
         </Row>
