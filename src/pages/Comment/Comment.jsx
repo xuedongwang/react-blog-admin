3fc9800ca26e111a08e7330a4b76157f06321ca9
Xuedong Wang <xuedongwang@tencent.com>
2020-12-14 18:26:16 +0800

    commit

--- src/pages/Comment/Comment.jsx
+++ src/pages/Comment/Comment.jsx
@@ -1,12 +1,78 @@
 import React, { Component } from 'react';
-import { PageHeader, Button, Card, Descriptions } from 'antd';
+import { PageHeader, Button, Popover, List, Skeleton, Avatar } from 'antd';
 
 class CreateArticle extends Component {
   constructor (props) {
     super(props);
-    this.state = {}
+    this.state = {
+      initLoading: false,
+      loading: false,
+      list: [
+        {
+          id: Math.random(),
+          name: Math.random(),
+          loading: false
+        },
+        {
+          id: Math.random(),
+          name: Math.random(),
+          loading: false
+        },
+        {
+          id: Math.random(),
+          name: Math.random(),
+          loading: false
+        }
+      ]
+    }
+    this.onLoadMore = this.onLoadMore.bind(this);
+  }
+  onLoadMore () {
+    const newList = [
+      {
+        id: Math.random(),
+        name: Math.random(),
+        loading: true
+      },
+      {
+        id: Math.random(),
+        name: Math.random(),
+        loading: true
+      },
+      {
+        id: Math.random(),
+        name: Math.random(),
+        loading: true
+      }
+    ];
+    this.setState({
+      list: this.state.list.concat(newList),
+      loading: true
+    })
+    setTimeout(() => {
+      this.setState({
+        list: this.state.list.map(item => ({...item, loading: false})),
+        loading: false
+      }, () => {
+        window.dispatchEvent(new Event('resize'));
+      })
+    }, 1000)
   }
   render () {
+    const { initLoading, loading, list } = this.state;
+    const loadMore =
+      !initLoading && !loading ? (
+        <div
+          style={{
+            textAlign: 'center',
+            marginTop: 12,
+            height: 32,
+            lineHeight: '32px',
+          }}
+        >
+          <Button onClick={this.onLoadMore}>加载更多</Button>
+        </div>
+      ) : null;
     return (
       <div>
         <PageHeader
@@ -14,22 +80,32 @@ class CreateArticle extends Component {
           onBack={() => window.history.back()}
           title="Title"
           subTitle="This is a subtitle"
-          extra={[
-            <Button key="3">保存草稿</Button>,
-            <Button key="2" type="primary">发布</Button>,
-          ]}
         >
-          <Card title="Card title">
-            <Descriptions title="User Info">
-              <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
-              <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-              <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-              <Descriptions.Item label="Remark">empty</Descriptions.Item>
-              <Descriptions.Item label="Address">
-                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-              </Descriptions.Item>
-            </Descriptions>
-          </Card>
+          <List
+            className="demo-loadmore-list"
+            loading={initLoading}
+            itemLayout="horizontal"
+            loadMore={loadMore}
+            dataSource={list}
+            renderItem={item => (
+              <List.Item
+                actions={[<a key="list-loadmore-edit">edit</a>, <a key="list-loadmore-more">more</a>]}
+              >
+                <Skeleton avatar title={false} loading={item.loading} active>
+                  <List.Item.Meta
+                    avatar={
+                      <Popover placement="rightTop" title="dasdasd" content="dasdasd" trigger="click">
+                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                      </Popover>
+                    }
+                    title={<a href="https://ant.design">{item.name}</a>}
+                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                  />
+                  <div>content</div>
+                </Skeleton>
+              </List.Item>
+            )}
+          />
         </PageHeader>
       </div>
     )
